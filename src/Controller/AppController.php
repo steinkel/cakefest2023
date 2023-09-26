85b2da20f1624112674acd3a747c36ae510ec494
Jorge Gonzalez <steinkel@gmail.com>
2023-09-26 07:44:36 -0700

    restore auth and enable form protection

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -42,11 +42,11 @@ class AppController extends Controller
         parent::initialize();
 
         $this->loadComponent('Flash');
-        //$this->loadComponent('Authentication.Authentication');
+        $this->loadComponent('Authentication.Authentication');
         /*
          * Enable the following component for recommended CakePHP form protection settings.
          * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
          */
-        //$this->loadComponent('FormProtection');
+        $this->loadComponent('FormProtection');
     }
 }
