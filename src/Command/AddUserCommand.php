96bfffa03047868280ab812bd10afe83a63a795a
Jorge Gonzalez <steinkel@gmail.com>
2023-09-26 06:06:30 -0700

    AddUser command

---
+++ src/Command/AddUserCommand.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Command;
+
+use Cake\Command\Command;
+use Cake\Console\Arguments;
+use Cake\Console\ConsoleIo;
+use Cake\Console\ConsoleOptionParser;
+use App\Model\Entity\User;
+
+/**
+ * AddUser command.
+ */
+class AddUserCommand extends Command
+{
+    /**
+     * Hook method for defining this command's option parser.
+     *
+     * @see https://book.cakephp.org/4/en/console-commands/commands.html#defining-arguments-and-options
+     * @param \Cake\Console\ConsoleOptionParser $parser The parser to be defined
+     * @return \Cake\Console\ConsoleOptionParser The built parser.
+     */
+    public function buildOptionParser(ConsoleOptionParser $parser): ConsoleOptionParser
+    {
+        $parser = parent::buildOptionParser($parser);
+
+        return $parser
+            ->addArgument('email', ['required' => true])
+            ->addArgument('password', ['required' => true]);
+    }
+
+    /**
+     * Implement this method with your command's logic.
+     *
+     * @param \Cake\Console\Arguments $args The command arguments.
+     * @param \Cake\Console\ConsoleIo $io The console io
+     * @return null|void|int The exit code or null for success
+     */
+    public function execute(Arguments $args, ConsoleIo $io)
+    {
+        $user = new User([
+            'email' => $args->getArgument('email'),
+            'password' => $args->getArgument('password'),
+        ]);
+        $this->fetchTable('Users')
+            ->saveOrFail($user);
+
+        return self::CODE_SUCCESS;
+    }
+}
